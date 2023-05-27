26d3614392271b4e7ad5ba3f0c4e0976e426034f
Mounajidev <lucilo@gmail.com>
2023-05-27 02:54:56 -0300

    update css

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+
+  import { Suspense, useState, useEffect } from "react";
+  import { useRouter } from "next/router";
+  import "../styles/globals.css";
+
+  function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+  
+    const [showing, setShowing] = useState(false);
+      const getLayout = Component.getLayout || (page => page);
+      useEffect(() => {
+        setShowing(true);
+      }, []);
+  
+      if (!showing) {
+        return null;
+      }
+  
+      if (typeof window === 'undefined') {
+        return <></>;
+      } else {
+  
+    return (
+       
+          
+      
+              <Component {...pageProps} />
+      
+    );
+              }
+  }
+  
+  export default MyApp;
+  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 
 import Image from 'next/image'
-import styles from '../src/app/page.module.css'
+
+// import '../styles/globals.css'
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 
