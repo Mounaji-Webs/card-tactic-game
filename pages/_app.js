32cbcdc01a1991b7d55d2a5c39bb4c406226b590
Mounajidev <lucilo@gmail.com>
2023-07-08 13:48:17 -0300

    update game version

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,25 @@
+import { Suspense, useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import "../styles/globals.css";
 
-  import { Suspense, useState, useEffect } from "react";
-  import { useRouter } from "next/router";
-  import "../styles/globals.css";
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
-  function MyApp({ Component, pageProps }) {
-    const router = useRouter();
-  
-    const [showing, setShowing] = useState(false);
-      const getLayout = Component.getLayout || (page => page);
-      useEffect(() => {
-        setShowing(true);
-      }, []);
-  
-      if (!showing) {
-        return null;
-      }
-  
-      if (typeof window === 'undefined') {
-        return <></>;
-      } else {
-  
-    return (
-       
-          
-      
-              <Component {...pageProps} />
-      
-    );
-              }
+  const [showing, setShowing] = useState(false);
+  const getLayout = Component.getLayout || ((page) => page);
+  useEffect(() => {
+    setShowing(true);
+  }, []);
+
+  if (!showing) {
+    return null;
   }
-  
-  export default MyApp;
-  
+
+  if (typeof window === "undefined") {
+    return <></>;
+  } else {
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
